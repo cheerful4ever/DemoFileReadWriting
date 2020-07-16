7fd5a5a51186ba0db9bf9a7274e5859ed0839d1b
18016375 <cecilialui16@yahoo.com.sg>
2020-07-16 12:42:49 +0800

    activities 1-10000000000000

--- app/src/main/java/sg/edu/rp/webservices/demofilereadwriting/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/demofilereadwriting/MainActivity.java
@@ -2,13 +2,52 @@ package sg.edu.rp.webservices.demofilereadwriting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    String folderLocation;
+    Button btnWrite, btnRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+        btnRead = (Button) findViewById(R.id.btnRead);
+
+        folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+
+        File folder = new File(folderLocation);
+        if (folder.exists() == false){
+            boolean result = folder.mkdir();
+            if (result == true){
+                Log.d("File Read/Write", "Folder created");
+            }
+        }
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+            }
+        });
     }
 }
